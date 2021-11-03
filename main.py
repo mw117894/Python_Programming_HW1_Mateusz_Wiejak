be625f13bad94ed033ba16439dcb8c4fcf0da836
MWiejak <mateuszwiejak0@gmail.com>
2021-11-03 21:59:09 +0100

    First unchecked version

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-def printFirstNNumbers(n):
+def printPrimeNumbers(n):
     for i in range(n + 1):
-        primeNumber = False
+        primeNumber = True
         if i > 1:
             for a in range(i):
                 if a > 1:
                     if i / a == i // a:
                         primeNumber = False
                         break
-                primeNumber = True
-        if primeNumber:
+        if primeNumber and i > 1:
             print(i)
-        else:
-            continue
-printFirstNNumbers(9)
-
