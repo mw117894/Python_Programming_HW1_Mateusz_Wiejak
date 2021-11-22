4157d318ac467e2ab384706c7fbf838ab883ea2d
MWiejak <mateuszwiejak0@gmail.com>
2021-11-22 09:21:40 +0100

    Final version

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 print("Input how many prime numbers you would like to print")
 n = int(input())
 primeNumbers = []
-i = 1
+i = 2
 while len(primeNumbers) < n:
     primeNumber = True
     for a in range(2,i-1):
         if i / a == i // a:
             primeNumber = False
             break
-    if primeNumber and i > 1:
+    if primeNumber:
         primeNumbers.append(i)
     i+=1
 print(primeNumbers)
